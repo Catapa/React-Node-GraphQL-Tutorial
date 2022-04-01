78aae7d5dc740b1a6abef8a7a21575693b11ebcf
Catapa <catalin24pap@gmail.com>
2022-04-01 19:53:30 +0300

    add 'books' field to AuthorType

--- Server/schema/schema.js
+++ Server/schema/schema.js
@@ -5,13 +5,18 @@ const { GraphQLObjectType,
     GraphQLString,
     GraphQLSchema,
     GraphQLID,
-    GraphQLInt} = graphql;
+    GraphQLInt,
+    GraphQLList
+} = graphql;
 
 // dummy data
 let books = [
     {name: 'Name of the wind', genre: 'Fantasy', id: '1', authorId: '1'},
     {name: 'The Final Empire', genre: 'Fantasy', id: '2', authorId: '2'},
-    {name: 'The Long Earth', genre: 'Sci-Fi', id: '3', authorId: '3'}
+    {name: 'The Long Earth', genre: 'Sci-Fi', id: '3', authorId: '3'},
+    {name: 'The Hero Of Ages', genre: 'Fantasy', id: '4', authorId: '2'},
+    {name: 'The Colour Of Magic', genre: 'Fantasy', id: '5', authorId: '3'},
+    {name: 'The Light Fantastic', genre: 'Fantasy', id: '6', authorId: '3'}
 ];
 
 let authors = [
@@ -41,7 +46,13 @@ const AuthorType = new GraphQLObjectType({
    fields: () => ({
        id: {type: GraphQLID},
        name: {type: GraphQLString},
-       age: {type: GraphQLInt}
+       age: {type: GraphQLInt},
+       books: {
+           type: new GraphQLList(BookType),
+           resolve(parent, args) {
+               return _.filter(books, {authorId: parent.id});
+           }
+       }
    })
 });
 
