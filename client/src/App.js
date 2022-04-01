23d37a35e8f2ad932e0a56d2baf0eb0408143ef3
Catapa <catalin24pap@gmail.com>
2022-04-02 00:17:57 +0300

    introduce BookList component

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
 
+//components
+import BookList from "./Components/BookList/BookList";
+
 function App() {
   return (
     <div>
         <h1>Alo</h1>
+        <BookList/>
     </div>
   );
 }
