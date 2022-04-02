7ed75f89d0056eb6cdfbfea5b5629465a634cd66
Catapa <catalin24pap@gmail.com>
2022-04-02 17:27:56 +0300

    create external file or gql queries

--- client/src/Components/AddBook/AddBook.js
+++ client/src/Components/AddBook/AddBook.js
@@ -1,16 +1,6 @@
 import React from 'react';
-import {gql} from 'apollo-boost';
-import {graphql} from 'react-apollo';
-
-const getAuthorsQuery = gql`
-    {
-        authors {
-            name
-            id
-        }
-    }
-`;
-
+import { graphql } from 'react-apollo';
+import { getAuthorsQuery } from "../../Queries/queries";
 
 const AddBook = (props) => {
     const displayAuthors = () => {
